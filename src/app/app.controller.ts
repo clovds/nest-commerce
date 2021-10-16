9aec35e7d23068d58f80244459ab2e477362d174
Wido Wicaksono <widowicaksono@wicaksono-2.local>
2021-10-16 16:36:35 +0700

    add app

---
+++ src/app/app.controller.ts
@@ -0,0 +1,33 @@
+import { Body, Controller, Delete, Get, HttpCode, HttpException, HttpStatus, Param, Post, Query } from '@nestjs/common';
+
+@Controller('app')
+export class AppController {
+    @Get()
+    async get(){
+        return 'Hello World!'
+    }
+
+    @Get('testing')
+    async testing(@Query('name') name: string, 
+    @Query('email') email: string){
+        return {name, email}
+    }
+
+    @Post('post')
+    @HttpCode(201)
+    async postTest(@Body('name') name: string, @Body('address') address: string): Promise<string> {
+        return `${name} and ${address}`
+    }
+
+    @Post('post2')
+    @HttpCode(201)
+    async postTest2(@Body() body): Promise<string> {
+        throw new HttpException('Forbidden Test', HttpStatus.FORBIDDEN)
+        return `${body.address}`
+    }
+
+    @Delete('delete/:name')
+    async delete(@Param('name') name: string){
+        return `delete ${name}`
+    }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose'
 import { AuthModule } from 'src/auth/auth.module';
 import { SharedModule } from 'src/shared/shared.module';
+import { AppService } from './app.service';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_URI), SharedModule, AuthModule]
+  imports: [MongooseModule.forRoot(process.env.MONGO_URI), SharedModule, AuthModule],
+  providers: [AppService],
+  controllers: [AppController]
 })
 export class AppModule {}
