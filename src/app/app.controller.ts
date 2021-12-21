245d5d02bb4ae6c4e702d9c758e96bb8e36cdb5e
Wido Wicaksono <wido@datasintesa.id>
2021-12-21 17:11:41 +0700

    update

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,33 +1,46 @@
-import { Body, Controller, Delete, Get, HttpCode, HttpException, HttpStatus, Param, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpException,
+  HttpStatus,
+  Param,
+  Post,
+  Query,
+} from '@nestjs/common';
 
-@Controller('app')
+@Controller()
 export class AppController {
-    @Get()
-    async get(){
-        return 'Hello World!'
-    }
+  @Get()
+  async get() {
+    return 'Hello World!';
+  }
 
-    @Get('testing')
-    async testing(@Query('name') name: string, 
-    @Query('email') email: string){
-        return {name, email}
-    }
+  @Get('testing')
+  async testing(@Query('name') name: string, @Query('email') email: string) {
+    return { name, email };
+  }
 
-    @Post('post')
-    @HttpCode(201)
-    async postTest(@Body('name') name: string, @Body('address') address: string): Promise<string> {
-        return `${name} and ${address}`
-    }
+  @Post('post')
+  @HttpCode(201)
+  async postTest(
+    @Body('name') name: string,
+    @Body('address') address: string,
+  ): Promise<string> {
+    return `${name} and ${address}`;
+  }
 
-    @Post('post2')
-    @HttpCode(201)
-    async postTest2(@Body() body): Promise<string> {
-        throw new HttpException('Forbidden Test', HttpStatus.FORBIDDEN)
-        return `${body.address}`
-    }
+  @Post('post2')
+  @HttpCode(201)
+  async postTest2(@Body() body): Promise<string> {
+    throw new HttpException('Forbidden Test', HttpStatus.FORBIDDEN);
+    return `${body.address}`;
+  }
 
-    @Delete('delete/:name')
-    async delete(@Param('name') name: string){
-        return `delete ${name}`
-    }
+  @Delete('delete/:name')
+  async delete(@Param('name') name: string) {
+    return `delete ${name}`;
+  }
 }
